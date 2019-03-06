763785ed3546c1e3eb5c83167ae1f41cee7e94ec
Nohan Budry <nohan.budry@icloud.com>
2019-03-06 20:28:23 +0100

    Implement FileTransformer

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
@@ -1,7 +1,5 @@
 package ch.heigvd.res.labio.impl.filters;
 
-import com.sun.xml.internal.fastinfoset.util.CharArray;
-
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
