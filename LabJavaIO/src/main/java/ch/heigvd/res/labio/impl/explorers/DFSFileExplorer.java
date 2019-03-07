2c0277eb2ed2371cf15219e936377c2c18e445b6
Nohan Budry <nohan.budry@icloud.com>
2019-03-07 10:29:27 +0100

    DFSFileExplorer sort files

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,7 @@ package ch.heigvd.res.labio.impl.explorers;
 import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.Arrays;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -15,9 +16,20 @@ import java.io.File;
 public class DFSFileExplorer implements IFileExplorer {
 
   @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
-    vistor.visit(rootDirectory);
-  }
+  public void explore(File rootDirectory, IFileVisitor visitor) {
 
+    visitor.visit(rootDirectory);
+
+    // Continue exploration
+    if (rootDirectory.isDirectory()) {
+
+      // TODO: ask about file order
+      File[] files = rootDirectory.listFiles();
+      Arrays.sort(files);
+
+      for (File file : files) {
+        explore(file, visitor);
+      }
+    }
+  }
 }
